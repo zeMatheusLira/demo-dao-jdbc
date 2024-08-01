82947e396e89238652f47d8e8883c9b90e1bdb00
Zé Matheus <josemw86@gmail.com>
2024-08-01 19:17:02 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,11 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "books");
-
-        Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
         // dessa forma o programa nao conhece a implementacao apenas a interface
         // é uma forma de fazer uma injecao de dependencia sem explicitar a implementacao
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(obj);
         System.out.println(seller);
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
 // a classe que vai expor o metodo que retorna o tipo da interface (SellerDao), mas
 //  internamente  vai instanciar a implementacao
 // macete para nao precisar expor a implementacao deixar somente a interface
