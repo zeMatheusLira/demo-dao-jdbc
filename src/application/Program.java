c28f4d9befa941824b71a7586d5ef7ff8316adb9
Zé Matheus <josemw86@gmail.com>
2024-08-02 16:50:00 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
         for (Seller obj: list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n === TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gamil.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = "+ newSeller.getId());
+
     }
 }
