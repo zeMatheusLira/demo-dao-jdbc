d0ac31b14bc9b8114d3be5320179a70f7a2f3ff9
Zé Matheus <josemw86@gmail.com>
2024-08-02 20:30:24 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,5 +40,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = "+ newSeller.getId());
 
+        System.out.println("\n === TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Bruce Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
+
+
     }
 }
